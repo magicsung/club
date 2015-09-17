240098e7ace8b61451f53d8455fbda7bdef61b76
Jack Sung <magicsung@gmail.com>
2015-09-17 11:24:02 +0800

    add CRUD

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -1,23 +1,62 @@
 class ClubsController < ApplicationController
+
+	before_action :set_event, :only => [ :show, :edit, :update, :destroy]
+
 	def index
   		@events = Event.all
+  		@events = Event.page(params[:page]).per(5)
 	end
 
 	def show
-		
+		@page_title = @event.name
 	end
 
 	def edit
-		
+	end
+
+	def update
+		flash[:notice] = "info was successfully updated"
+
+  		if @event.update(event_params)
+			redirect_to :action => :show, :id => @event
+		else
+			render :action => :edit
+		end
 	end
 
 	def destroy
-		
+		flash[:alert] = "info was successfully deleted"
+
+		@event.destroy
+
+		redirect_to :action => :index
 	end
 
 	def new
-		
+		@event = Event.new
+	end
+
+	def create
+		flash[:notice] = "info was successfully created"
+
+		@event = Event.new(event_params)
+
+		if @event.save
+	    	redirect_to :action => :index
+		else
+	    	render :action => :new
+	    end
+	end
+
+
+	private
+
+	def event_params
+		params.require(:event).permit(:name, :description)
+	end
+
+	def set_event
+		@event = Event.find(params[:id])
 	end
 
 end
-
